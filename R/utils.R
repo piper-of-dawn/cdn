0c4e37d059de9ba528b00e50eceba5bc63a16b0f
Kumar Shantanu <69080662+piper-of-dawn@users.noreply.github.com>
2024-06-10 12:02:40 +0200

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -6,4 +6,13 @@ save_as_rds <- function(object, file_path) {
     cat("Error occurred while saving the object:", e$message, "\n")
   })
 }
+read_json_as_character <- function(file_path) {
+  # Read the JSON file
+  json_data <- fromJSON(file_path, simplifyVector = FALSE)
+  
+  # Convert the JSON object to a character vector
+  json_string <- toJSON(json_data, pretty = TRUE)
+  
+  return(json_string)
+}
 
