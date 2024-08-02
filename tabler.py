bdcdfdccf3b3c5f548cef87a74b3ddec912af614
Kumar Shantanu <69080662+piper-of-dawn@users.noreply.github.com>
2024-08-02 08:21:58 +0200

    Update tabler.py

--- tabler.py
+++ tabler.py
@@ -1,3 +1,7 @@
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill, Border, Side, Alignment
+
 def int_to_roman(num):
     if not 1 <= num <= 3999:
         raise ValueError("Number out of range (must be between 1 and 3999)")
@@ -80,3 +84,62 @@ def dict_to_dataframe(data_dict, column_names=["Column 1", "Column 2"], remove_l
         df = df.applymap(lambda x: remove_latex(str(x)) if isinstance(x, str) else x)
     
     return df
+
+
+
+
+def df_to_excel(df, file_name, title=None):
+    # Create a workbook and select the active worksheet
+    wb = Workbook()
+    ws = wb.active
+    
+    # Define styles
+    header_font = Font(name='Consolas', bold=True)
+    cell_font = Font(name='Consolas')
+    white_fill = PatternFill(start_color='FFFFFF', end_color='FFFFFF', fill_type='solid')
+    thick_border = Border(top=Side(style='thick'), bottom=Side(style='thick'))
+    thin_bottom_border = Border(bottom=Side(style='thin'))
+    alignment = Alignment(horizontal='center', vertical='center')
+    
+    # Adjust row height
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.font = cell_font
+            cell.fill = white_fill
+            cell.border = thin_bottom_border
+            ws.row_dimensions[cell.row].height = 21
+    
+    # Add title if provided
+    start_row = 1
+    if title:
+        ws.merge_cells(start_row=start_row, start_column=1, end_row=start_row, end_column=len(df.columns))
+        title_cell = ws.cell(row=start_row, column=1)
+        title_cell.value = title
+        title_cell.font = Font(name='Consolas', bold=True, size=14)
+        title_cell.alignment = alignment
+        start_row += 1
+
+    # Add header
+    for col_num, column_title in enumerate(df.columns, 1):
+        cell = ws.cell(row=start_row, column=col_num)
+        cell.value = column_title
+        cell.font = header_font
+        cell.fill = white_fill
+        cell.border = thick_border
+        cell.alignment = alignment
+        ws.row_dimensions[start_row].height = 21
+
+    # Add data rows
+    for row_num, row_data in enumerate(df.values, start=start_row + 1):
+        for col_num, cell_value in enumerate(row_data, 1):
+            cell = ws.cell(row=row_num, column=col_num)
+            cell.value = cell_value
+            cell.font = cell_font
+            cell.fill = white_fill
+            cell.border = thin_bottom_border
+            cell.alignment = alignment
+        ws.row_dimensions[row_num].height = 21
+
+    # Save the workbook
+    wb.save(file_name)
+
