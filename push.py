8887ac3e4275acfe13c135f7970eb0e148c25d30
piper_of_the_dawn <kumarshan25@gmail.com>
2024-10-31 12:00:19 +0100

    Usual commit

--- push.py
+++ push.py
@@ -1,25 +1,12 @@
 import os
 
 def add_commit_push():
-    # Navigate to the project directory
-    # os.chdir('C:\git\cdn')
-    
-    # Check if git is initialized in the project directory
-    # try:
-    #     with open('.git', 'r') as _:
-    #         pass  # File exists, ignore it
-    # except FileNotFoundError:
-    #     print("Error: This doesn't appear to be a Git repository.")
-    #     return
-    
-    # Add file board.txt
-    os.system('echo "Hello World!" > board.txt')
-    
+   
     # Stage the changes
     os.system('git add .')
-    
+
     # Commit the changes
-    os.system('git commit -m "Added board.txt"')
+    os.system('git commit -m "Usual commit"')
     
     # Push the changes to the remote repository
     os.system('git push origin main')
